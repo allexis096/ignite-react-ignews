e95991dab17072a95e574ae1ddbc9b5e875a9ea2
allexis096 <allexissoares@gmail.com>
2021-04-02 12:26:34 -0300

    :zap: feat: improving performance using authenticating methods

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -39,7 +39,14 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
 
   const slug = params!.slug;
 
-  // if (!session) {}
+  if (!session!.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
   const prismic = getPrismicClient(req);
 
